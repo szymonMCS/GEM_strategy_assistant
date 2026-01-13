953715311151b6075f9f7aaea2b9529c7aab800d
szymonMCS <szymon.maciejewski.programista@gmail.com>
2026-01-13 23:08:43 +0100

    config file for api and market sites created

---
+++ src/momentum_assistant/config/__init__.py
@@ -0,0 +1,11 @@
+from .settings import settings, get_settings, Settings
+from .constants import get_stooq_link, get_yfinance_ticker, get_stooq_ticker
+
+__all__ = [
+    "settings", 
+    "get_settings", 
+    "Settings",
+    "get_stooq_link",
+    "get_yfinance_ticker",
+    "get_stooq_ticker"
+]
